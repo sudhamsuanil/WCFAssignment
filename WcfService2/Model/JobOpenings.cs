f8718d920a7a3f6938535a596e3c726ad3d7d757
sudhamsuanil <55593152+sudhamsuanil@users.noreply.github.com>
2019-09-20 22:45:22 +0530

    Add project files.

---
+++ WcfService2/Model/JobOpenings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WcfService2.Model
+{
+    public class JobOpenings
+    {   
+        public string roleName { get; set; }
+        public string experience { get; set; }
+        public string roleDescription { get; set; }
+
+    }
+}
---
+++ WcfService2/Service1.svc.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using WcfService2.Model;
+
+namespace WcfService2
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        static List<JobOpenings> jobOpen = new List<JobOpenings>();
+      
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public Service1()
+        {
+            jobOpen.Add(new JobOpenings { roleName = "Architect", experience = "10 years", roleDescription = "Responsible as solution architect" });
+            jobOpen.Add(new JobOpenings { roleName = "Developer", experience = "5 years", roleDescription = ".net developer" });
+            jobOpen.Add(new JobOpenings { roleName = "Tester", experience = "3 years", roleDescription = "Selenium testing" });
+
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+
+        public List<JobOpenings> OpeningJobs()
+        {
+           
+            return jobOpen;
+        }
+
+        public List<JobOpenings> OpeningJobsByRole(string roleName)
+        {
+            return jobOpen.Where(x => x.roleName.ToLower() == roleName.ToLower()).ToList();
+        }
+    }
+}
